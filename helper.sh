d902f21d8cc452706a105e98d8d387a399ad15a4
Wouter Saelens <wouter.saelens@gmail.com>
2019-04-17 13:42:58 +0200

    build for master & devel

--- helper.sh
+++ helper.sh
@@ -15,12 +15,12 @@ upload_tar() {
   tar -cvzf - * | split -b 20M - "cache.tar.gz.part-"
 
   git init
-  git checkout --orphan cache-$TRAVIS_BRANCH
+  git checkout --orphan cache-$DYNVERSE_BRANCH
 
   git add cache.tar.gz*
   git commit --allow-empty -m "Travis build: $TRAVIS_BUILD_NUMBER [ci skip]"
 
   git remote add origin https://${GITHUB_PAT}@github.com/dynverse/travis_package_cacher.git
-  git push --force origin cache-$TRAVIS_BRANCH
+  git push --force origin cache-$DYNVERSE_BRANCH
   popd
 }
